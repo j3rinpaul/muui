8ffe19e0e0b5fd5dd083aeb62c16d1c18ff53421
j3rin <jerrymic2255@gmail.com>
2023-01-23 20:01:27 +0530

    news

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -5,7 +5,7 @@ export const Typo = () => {
     <div>
         <Typography variant='h1'>Hello World</Typography>
         <Typography variant='h2'>Hello World</Typography>
-        <Typography variant='h3'>Hello World</Typography>
+        <Typography variant='h3'>New World</Typography>
         <Typography variant='h4'>Hello World</Typography>
         <Typography variant='h5'>Hello World</Typography>
         <Typography variant='h6'>Hello World</Typography>
